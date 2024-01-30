e1d14267ce961fb2b04da0771a77c00f2cff2011
Cagdas <kocamancagdas59@gmail.com>
2024-01-30 23:20:27 +0300

    bug fixed

--- src/components/Configuration.tsx
+++ src/components/Configuration.tsx
@@ -16,33 +16,31 @@ export default function Configuration() {
             <button onClick={() => setActive(state => !state)} className="z-50 absolute right-0 mt-2 mr-2 ring-1 ring-white/20 bg-white/5 hover:bg-white/10 rounded-xl p-2 flex items-center justify-center active:scale-95">
                 <svg className="fill-white" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 16c2.206 0 4-1.794 4-4s-1.794-4-4-4-4 1.794-4 4 1.794 4 4 4zm0-6c1.084 0 2 .916 2 2s-.916 2-2 2-2-.916-2-2 .916-2 2-2z"></path><path d="m2.845 16.136 1 1.73c.531.917 1.809 1.261 2.73.73l.529-.306A8.1 8.1 0 0 0 9 19.402V20c0 1.103.897 2 2 2h2c1.103 0 2-.897 2-2v-.598a8.132 8.132 0 0 0 1.896-1.111l.529.306c.923.53 2.198.188 2.731-.731l.999-1.729a2.001 2.001 0 0 0-.731-2.732l-.505-.292a7.718 7.718 0 0 0 0-2.224l.505-.292a2.002 2.002 0 0 0 .731-2.732l-.999-1.729c-.531-.92-1.808-1.265-2.731-.732l-.529.306A8.1 8.1 0 0 0 15 4.598V4c0-1.103-.897-2-2-2h-2c-1.103 0-2 .897-2 2v.598a8.132 8.132 0 0 0-1.896 1.111l-.529-.306c-.924-.531-2.2-.187-2.731.732l-.999 1.729a2.001 2.001 0 0 0 .731 2.732l.505.292a7.683 7.683 0 0 0 0 2.223l-.505.292a2.003 2.003 0 0 0-.731 2.733zm3.326-2.758A5.703 5.703 0 0 1 6 12c0-.462.058-.926.17-1.378a.999.999 0 0 0-.47-1.108l-1.123-.65.998-1.729 1.145.662a.997.997 0 0 0 1.188-.142 6.071 6.071 0 0 1 2.384-1.399A1 1 0 0 0 11 5.3V4h2v1.3a1 1 0 0 0 .708.956 6.083 6.083 0 0 1 2.384 1.399.999.999 0 0 0 1.188.142l1.144-.661 1 1.729-1.124.649a1 1 0 0 0-.47 1.108c.112.452.17.916.17 1.378 0 .461-.058.925-.171 1.378a1 1 0 0 0 .471 1.108l1.123.649-.998 1.729-1.145-.661a.996.996 0 0 0-1.188.142 6.071 6.071 0 0 1-2.384 1.399A1 1 0 0 0 13 18.7l.002 1.3H11v-1.3a1 1 0 0 0-.708-.956 6.083 6.083 0 0 1-2.384-1.399.992.992 0 0 0-1.188-.141l-1.144.662-1-1.729 1.124-.651a1 1 0 0 0 .471-1.108z"></path></svg>
             </button>
-            {active && (
-                <div className="fixed top-0 right-0 w-72 h-full bg-black z-40 backdrop-blur">
-                    <h2 className="text-center text-white text-3xl pt-2">Ayarlar</h2>
-                    <div className="w-full h-full flex flex-col items-center mt-5 gap-y-5">
-                        <div className="flex flex-col gap-y-2 bg-white/10 rounded p-3">
-                            <span className="text-white text-center text-sm">Deri Dokular</span>
-                            <div className="grid grid-cols-12 gap-3">
-                                {leathers.map((leather) => (
-                                    <button title={leather.title} key={leather.key} onClick={() => leatherTextureStore.change(leather.key)} className={"w-14 h-14 col-span-4 rounded-full " + (leatherTextureStore.key === leather.key ? " ring-2 ring-white" : "")}>
-                                        <img draggable={false} className="object-cover rounded-full w-full h-full" src={leather.material} alt={leather.title} />
-                                    </button>
-                                ))}
-                            </div>
+            <div className={"fixed top-0 w-72 h-full bg-black z-40 backdrop-blur ease-in-out duration-200" + (active ? ' right-0' : ' -right-96')}>
+                <h2 className="text-center text-white text-3xl pt-2">Ayarlar</h2>
+                <div className="w-full h-full flex flex-col items-center mt-5 gap-y-5">
+                    <div className="flex flex-col gap-y-2 bg-white/10 rounded p-3">
+                        <span className="text-white text-center text-sm">Deri Dokular</span>
+                        <div className="grid grid-cols-12 gap-3">
+                            {leathers.map((leather) => (
+                                <button title={leather.title} key={leather.key} onClick={() => leatherTextureStore.change(leather.key)} className={"w-14 h-14 col-span-4 rounded-full " + (leatherTextureStore.key === leather.key ? " ring-2 ring-white" : "")}>
+                                    <img draggable={false} className="object-cover rounded-full w-full h-full" src={leather.material} alt={leather.title} />
+                                </button>
+                            ))}
                         </div>
-                        <div className="flex flex-col gap-y-2 bg-white/10 rounded p-3">
-                            <span className="text-white text-center text-sm">Minder</span>
-                            <div className="grid grid-cols-12 gap-3">
-                                {fabrics.map((fabric) => (
-                                    <button title={fabric.title} key={fabric.key} onClick={() => fabricTextureStore.change(fabric.key)} className={"w-14 h-14 col-span-4 rounded-full " + (fabricTextureStore.key === fabric.key ? " ring-2 ring-white" : "")}>
-                                        <img draggable={false} className="object-cover rounded-full w-full h-full" src={fabric.material} alt={fabric.title} />
-                                    </button>
-                                ))}
-                            </div>
+                    </div>
+                    <div className="flex flex-col gap-y-2 bg-white/10 rounded p-3">
+                        <span className="text-white text-center text-sm">Minder</span>
+                        <div className="grid grid-cols-12 gap-3">
+                            {fabrics.map((fabric) => (
+                                <button title={fabric.title} key={fabric.key} onClick={() => fabricTextureStore.change(fabric.key)} className={"w-14 h-14 col-span-4 rounded-full " + (fabricTextureStore.key === fabric.key ? " ring-2 ring-white" : "")}>
+                                    <img draggable={false} className="object-cover rounded-full w-full h-full" src={fabric.material} alt={fabric.title} />
+                                </button>
+                            ))}
                         </div>
                     </div>
                 </div>
-            )}
+            </div>
         </>
     )
 }
